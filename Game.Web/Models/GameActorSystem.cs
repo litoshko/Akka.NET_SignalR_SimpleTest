5a461dcd43a44fe18913df2386b3174414a240fb
Andrii Litoshko <xxoro.lan@gmail.com>
2016-03-13 22:33:00 +0200

    Configure SignalR and its communitation with Akka.NET

--- Game.Web/Models/GameActorSystem.cs
+++ Game.Web/Models/GameActorSystem.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using Akka.Actor;
 using Game.ActorModel.Actors;
+using Game.ActorModel.ExternalSystems;
 
 namespace Game.Web.Models
 {
     public static class GameActorSystem
     {
         private static ActorSystem ActorSystem;
+        private static IGameEventsPusher _gameEventsPusher;
 
         public static void Create()
         {
+            _gameEventsPusher = new SignalRGameEventPusher();
+
             ActorSystem = Akka.Actor.ActorSystem.Create("GameSystem");
 
-            ActorReferences.GameController = 
+            ActorReferences.GameController =
                 ActorSystem.ActorOf<GameControllerActor>();
+
+            ActorReferences.SignalRBridge =
+                ActorSystem.ActorOf(
+                    Props.Create(() =>
+                        new SignalRBridgeActor(
+                            _gameEventsPusher,
+                            ActorReferences.GameController)),
+                    "SignalRBridge");
         }
 
         public static void Shutdown()
@@ -27,6 +39,7 @@ namespace Game.Web.Models
         public static class ActorReferences
         {
             public static IActorRef GameController { get; set; }
+            public static IActorRef SignalRBridge { get; set; }
         }
     }
 }
